0ae76a37259af924fa9cc49aefa5e0e89578d1ea
AL-FAHAMI <altoihir@gmail.com>
2025-01-26 14:51:20 +0100

    feat: wbeClient bean definition

--- exchangerateservice/src/main/java/com/codelogium/exchangerateservice/config/AppConfig.java
+++ exchangerateservice/src/main/java/com/codelogium/exchangerateservice/config/AppConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
