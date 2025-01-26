8081b522f614940f2ad71401671d28c2b4ea045f
AL-FAHAMI <altoihir@gmail.com>
2025-01-26 14:48:15 +0100

    feat: define beans and load context manually

--- exchangerateservice/src/main/java/com/codelogium/exchangerateservice/ExchangerateserviceApplication.java
+++ exchangerateservice/src/main/java/com/codelogium/exchangerateservice/ExchangerateserviceApplication.java
@@ -2,11 +2,34 @@ package com.codelogium.exchangerateservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.codelogium.exchangerateservice.config.AppConfig;
 
 @SpringBootApplication
 public class ExchangerateserviceApplication {
 
+	
+	
 	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		
+		//Register the config class
+		context.register(AppConfig.class);
+
+		//Refressh the context so the bean can be load
+		context.refresh();
+
+		//Retrieve the AppConfig bean
+		AppConfig appConfig = context.getBean(AppConfig.class);
+
+		//Get the base url from the bean
+		String baseUrl = appConfig.getBaseUrl();
+
+		System.out.println("Base Url: " + baseUrl);
+
+		context.close();
 		SpringApplication.run(ExchangerateserviceApplication.class, args);
 	}
 
--- exchangerateservice/src/main/java/com/codelogium/exchangerateservice/config/AppConfig.java
+++ exchangerateservice/src/main/java/com/codelogium/exchangerateservice/config/AppConfig.java
@@ -1,10 +1,43 @@
 package com.codelogium.exchangerateservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+//Explicitly loading the properties to the context 
+@PropertySource("classpath:application.properties") 
 public class AppConfig {
 
-    
+    @Value("$external-api.base-url")
+    private String baseUrl;
+
+    /*
+     * As we will configure Spring context manually (e.g., using AnnotationConfigApplicationContext), we need to declare WebClient.Builder bean to avoid Spring may not have enabled the automatic configuration for WebClient.Builder.
+     */
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+    // This bean is necessary to resolve placeholders in @Value annotations
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder
+                    .baseUrl(baseUrl)
+                    .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                    .build();
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    public String getBaseUrl() { return this.baseUrl; }
     
 }
