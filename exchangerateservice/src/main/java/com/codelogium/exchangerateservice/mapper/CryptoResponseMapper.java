007da157564abc25c2de2da5ae77e09f9ec4374b
AL-FAHAMI <altoihir@gmail.com>
2025-01-31 23:39:13 +0100

    feat: add empty constructor for proper serialization

--- exchangerateservice/src/main/java/com/codelogium/exchangerateservice/mapper/CryptoResponseMapper.java
+++ exchangerateservice/src/main/java/com/codelogium/exchangerateservice/mapper/CryptoResponseMapper.java
@@ -2,13 +2,18 @@ package com.codelogium.exchangerateservice.mapper;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class CryptoResponseMapper {
     private String symbol;
     private String base;
+    @JsonProperty("price")
     private BigDecimal price;
 }
