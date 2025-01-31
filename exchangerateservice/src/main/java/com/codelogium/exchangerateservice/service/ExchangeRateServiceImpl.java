198b6ef512e8a9abc7baadcabc6d91528b60cb94
AL-FAHAMI <altoihir@gmail.com>
2025-01-31 13:44:24 +0100

    fix: use of constructor injection

--- exchangerateservice/src/main/java/com/codelogium/exchangerateservice/service/ExchangeRateServiceImpl.java
+++ exchangerateservice/src/main/java/com/codelogium/exchangerateservice/service/ExchangeRateServiceImpl.java
@@ -1,6 +1,5 @@
 package com.codelogium.exchangerateservice.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
