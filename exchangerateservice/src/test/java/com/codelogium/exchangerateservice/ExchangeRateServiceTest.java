6f22b9fba1c70713a807a0fffb826fad345f1f8d
AL-FAHAMI <altoihir@gmail.com>
2025-02-01 00:22:58 +0100

    fix: minor changes

--- exchangerateservice/src/test/java/com/codelogium/exchangerateservice/ExchangeRateServiceTest.java
+++ exchangerateservice/src/test/java/com/codelogium/exchangerateservice/ExchangeRateServiceTest.java
@@ -5,9 +5,7 @@ import java.math.BigDecimal;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
