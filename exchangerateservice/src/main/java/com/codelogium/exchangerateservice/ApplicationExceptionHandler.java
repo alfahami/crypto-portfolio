14b601d87b21008ed694b6e6de8a587bf4b67c98
AL-FAHAMI <altoihir@gmail.com>
2025-02-04 16:30:27 +0100

    refractor: rm unused imports

--- exchangerateservice/src/main/java/com/codelogium/exchangerateservice/ApplicationExceptionHandler.java
+++ exchangerateservice/src/main/java/com/codelogium/exchangerateservice/ApplicationExceptionHandler.java
@@ -1,10 +1,5 @@
 package com.codelogium.exchangerateservice;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
