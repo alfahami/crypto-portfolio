d8a42ba3c1a97d33335f4b3e777cb80f880870ed
AL-FAHAMI <altoihir@gmail.com>
2025-02-04 16:14:52 +0100

    feat: implement handleErrorResponse from exception class

--- exchangerateservice/src/main/java/com/codelogium/exchangerateservice/exception/CryptoException.java
+++ exchangerateservice/src/main/java/com/codelogium/exchangerateservice/exception/CryptoException.java
@@ -1,45 +1,43 @@
 
 package com.codelogium.exchangerateservice.exception;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.reactive.function.client.ClientResponse;
 
 import com.fasterxml.jackson.databind.JsonNode;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import reactor.core.publisher.Mono;
 
 @Getter
+
 public class CryptoException extends RuntimeException {
 
     private final HttpStatusCode status;
+    private String message;
 
     public CryptoException(HttpStatusCode status, String message) {
-        super(message);
+        this.message = message;
         this.status = status;
     }
 
-    // Factory method of creating an instance of this from a clientResponse
-    public static Mono<CryptoException> from(ClientResponse response) {
-        return response.bodyToMono(JsonNode.class)
-                   .map(errorBody -> new CryptoException(response.statusCode(), "API Error: " + errorBody));
-    }
+    public static Mono<Throwable> handleErrorResponse(ClientResponse clientResponse, String base) {
+        // Extract the body of the error response as a Json
+        return clientResponse.bodyToMono(JsonNode.class)
+                .flatMap(errorBody -> {
 
-    public static boolean isClientError(Throwable throwable) {
-        return throwable instanceof CryptoException && 
-        ((CryptoException) throwable).isClientError();
-    }
-
-    public static boolean is4xxServerError(Throwable throwable) {
-        return throwable instanceof CryptoException &&
-        ((CryptoException) throwable).isServerError();
-    }
-
-    public boolean isClientError() {
-        return status.is4xxClientError();
-    }
-
-    public boolean isServerError() {
-        return status.is5xxServerError();
+                    // Extract error details dynamically
+                    JsonNode statusNode = errorBody.path("status");
+                    String errorMessage = statusNode.path("error_message").asText();
+                    //Check if invalid base
+                    if (errorMessage.toLowerCase().contains("convert")) {
+                        return Mono.error(new CryptoException(
+                                clientResponse.statusCode(),
+                                "Invalid Base Currency: " + base + " is not supported."));
+                    }
+                    // Return a Mono error with a custom exception that contains the error message
+                    return Mono.error(new CryptoException(clientResponse.statusCode(), errorMessage));
+                });
     }
 }
--- exchangerateservice/src/main/java/com/codelogium/exchangerateservice/service/ExchangeRateServiceImpl.java
+++ exchangerateservice/src/main/java/com/codelogium/exchangerateservice/service/ExchangeRateServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.codelogium.exchangerateservice.exception.CryptoException;
 import com.codelogium.exchangerateservice.mapper.CryptoResponseMapper;
 import com.fasterxml.jackson.databind.JsonNode;
-
 import lombok.AllArgsConstructor;
 import reactor.core.publisher.Mono;
 
@@ -34,42 +33,36 @@ public class ExchangeRateServiceImpl implements ExchangeRateService {
     public Mono<CryptoResponseMapper> retrivePrice(String symbol, String base) {
         return webClient.get().uri(
                 uriBuilder -> uriBuilder.path("/v1/cryptocurrency/quotes/latest")
-                        .queryParam("symbol", symbol)
-                        .queryParam("convert", base)
-                        .build())
+                .queryParam("symbol", symbol)
+                .queryParam("convert", base)
+                .build())
                 .retrieve()
-                .onStatus( 
-                        HttpStatusCode::isError, 
-                        clientResponse -> CryptoException.from(clientResponse).flatMap(Mono::error)) // wraps a CryptoException created from the ClientResponse object 
+                .onStatus(HttpStatusCode::isError, clientResponse -> CryptoException.handleErrorResponse(clientResponse, base)) 
                 .bodyToMono(JsonNode.class)
                 .map(response -> {
                     JsonNode quote = response.path("data").path(symbol).path("quote").path(base);
 
                     BigDecimal price = quote.has("price") ? quote.get("price").decimalValue() : BigDecimal.ZERO;
-                    //when invalid symbol, cmc respond with price as 0 -> we're explicitely handling that error by checking if the price is zero 
+                    //explicitely handling base error as cmc give response with 0 price value 
                     if(price.compareTo(BigDecimal.ZERO) == 0) throw new CryptoException(HttpStatus.BAD_REQUEST, "Invalid Symbol: " + symbol);
-                    return new CryptoResponseMapper(
-                            symbol,
-                            base,
-                            price);
+                    
+                    return new CryptoResponseMapper(symbol,base,price);
                 });
     }
 
-        @Override
-        public Mono<ResponseEntity<String>> getAllData() {
-                /*
-                 * Asynchronously fetches cryptocurrency listings using WebClient, with error
-                 * handling and reactive response processing
-                 */
-                Mono<ResponseEntity<String>> result = webClient.get()
-                                .uri("/v1/cryptocurrency/listings/latest")
-                                .retrieve()
-                                .onStatus( 
-                        HttpStatusCode::isError, 
-                        clientResponse -> CryptoException.from(clientResponse).flatMap(Mono::error)) // wraps a CryptoException created from the ClientResponse object 
-                        .toEntity(String.class)
-                        .timeout(Duration.ofMillis(10000));
-
-                return result;
-        }
+     /*
+    * Asynchronously fetches cryptocurrency listings using WebClient, with error handling and reactive response processing
+    */
+    @Override
+    public Mono<ResponseEntity<String>> getAllData() {
+        Mono<ResponseEntity<String>> result = webClient.get()
+            .uri("/v1/cryptocurrency/listings/latest")
+            .retrieve()
+            .onStatus( 
+                HttpStatusCode::isError, 
+                clientResponse -> CryptoException.handleErrorResponse(clientResponse, null))
+            .toEntity(String.class)
+            .timeout(Duration.ofMillis(10000));
+        return result;
+    }    
 }
