850aaaf5b1a69e58ccff16f86d02b3c09b13a90e
AL-FAHAMI <altoihir@gmail.com>
2025-02-04 17:03:03 +0100

    feat: override handle missing params

--- exchangerateservice/src/main/java/com/codelogium/exchangerateservice/ApplicationExceptionHandler.java
+++ exchangerateservice/src/main/java/com/codelogium/exchangerateservice/ApplicationExceptionHandler.java
@@ -1,18 +1,33 @@
 package com.codelogium.exchangerateservice;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.server.MissingRequestValueException;
+import org.springframework.web.server.ServerWebExchange;
 
 import com.codelogium.exchangerateservice.exception.CryptoException;
 import com.codelogium.exchangerateservice.exception.ErrorResponse;
 
+import reactor.core.publisher.Mono;
+
 @RestControllerAdvice
-public class ApplicationExceptionHandler {
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CryptoException.class)
     public ResponseEntity<Object> handleCryptoException(CryptoException ex) {
-        ErrorResponse error = new ErrorResponse(ex.getMessage());
+        ErrorResponse error = new ErrorResponse(ex.getLocalizedMessage());
         return new ResponseEntity<>(error, ex.getStatus());   
+    }
+    
+    @Override
+    protected Mono<ResponseEntity<Object>> handleMissingRequestValueException(MissingRequestValueException ex,
+            HttpHeaders headers, HttpStatusCode status, ServerWebExchange exchange) {
+
+                ErrorResponse error = new ErrorResponse(ex.getReason());
+                return Mono.just(ResponseEntity.badRequest().body(error));
     }
 }
