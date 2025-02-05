daa6dfc052e648afce1faafe0e6b6f70cbba2075
AL-FAHAMI <altoihir@gmail.com>
2025-02-05 12:20:58 +0100

    feat: add holding with initialization check

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/Portfolio.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/Portfolio.java
@@ -1,10 +1,30 @@
 package com.codelogium.portfolioservice.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+@Data
 @Entity
+@RequiredArgsConstructor
 @Table(name = "portfolios")
 public class Portfolio {
-    
+    private Long id;
+    private String name;
+
+    private User user;
+    private List<Holding> holdings;
+
+    //Add holding to the portfolio with initialization check
+    public void addHolding(Holding holding) {
+        if(this.holdings == null) {
+            this.holdings = new ArrayList<>();
+        }
+        this.holdings.add(holding);
+    }
+
 }
