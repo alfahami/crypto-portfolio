d782bc9c02234282b565b8e27ebe0e2dd3969c34
AL-FAHAMI <altoihir@gmail.com>
2025-02-05 12:27:53 +0100

    feat: add fields to entity

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/Holding.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/Holding.java
@@ -1,10 +1,22 @@
 package com.codelogium.portfolioservice.entity;
 
+import java.math.BigDecimal;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+@Data
 @Entity
+@RequiredArgsConstructor
 @Table(name = "holdings")
 public class Holding {
-    
+
+    private Long id;
+    private String symbol;
+    private BigDecimal amount;
+
+    private Set<Portfolio> portfolios;
 }
--- portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/User.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/User.java
@@ -1,10 +1,21 @@
 package com.codelogium.portfolioservice.entity;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+@Data
 @Entity
+@RequiredArgsConstructor
 @Table(name = "users")
 public class User {
-    
+
+    private Long id;
+    private String fullName;
+    private LocalDate birthDate;
+    private String profession;
+
 }
