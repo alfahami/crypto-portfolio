c2f2396c37c52820b997a56dd0f545510cb58366
AL-FAHAMI <altoihir@gmail.com>
2025-02-05 12:10:24 +0100

    refractor: add entity base classes

---
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/Holding.java
@@ -0,0 +1,10 @@
+package com.codelogium.portfolioservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "holdings")
+public class Holding {
+    
+}
