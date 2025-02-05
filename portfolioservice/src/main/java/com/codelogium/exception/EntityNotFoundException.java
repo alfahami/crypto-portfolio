008ba1d3d06aa46fc131309664ac84d869fd976b
AL-FAHAMI <altoihir@gmail.com>
2025-02-05 20:49:06 +0100

    fix: correc id type definition

--- portfolioservice/src/main/java/com/codelogium/exception/EntityNotFoundException.java
+++ portfolioservice/src/main/java/com/codelogium/exception/EntityNotFoundException.java
@@ -2,7 +2,7 @@ package com.codelogium.exception;
 
 public class EntityNotFoundException extends RuntimeException {
     
-    public EntityNotFoundException(String id, Class<?> entityClass) {
+    public EntityNotFoundException(Long id, Class<?> entityClass) {
         super("The " + entityClass.getSimpleName().toLowerCase() + " with the id " + id + " is not found in our records");
     }
 }
