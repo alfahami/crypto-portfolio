34b2683f17e72a53ea00bb61975369fc1dab68b7
AL-FAHAMI <altoihir@gmail.com>
2025-02-06 03:34:40 +0100

    feat: implement add user service

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/service/UserService.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/service/UserService.java
@@ -1,5 +1,7 @@
 package com.codelogium.portfolioservice.service;
 
+import com.codelogium.portfolioservice.entity.User;
+
 public interface UserService {
-  
+    User addUser(User newUser);
 } 
--- portfolioservice/src/main/java/com/codelogium/portfolioservice/service/UserServiceImp.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/service/UserServiceImp.java
@@ -2,10 +2,21 @@ package com.codelogium.portfolioservice.service;
 
 import org.springframework.stereotype.Service;
 
+import com.codelogium.portfolioservice.entity.User;
+import com.codelogium.portfolioservice.respositry.UserRepository;
+
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImp implements UserService {
     
+    private UserRepository userRepository;
+
+    @Override
+    public User addUser(User newUser) {
+        return userRepository.save(newUser);
+    }
+    
+
 }
