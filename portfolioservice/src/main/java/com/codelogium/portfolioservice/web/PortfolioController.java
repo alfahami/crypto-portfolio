59fadf86ba6e1a065323f0475cbb78d12fd13e60
AL-FAHAMI <altoihir@gmail.com>
2025-02-06 03:11:34 +0100

    feat: add save portfolio handler

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/web/PortfolioController.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/web/PortfolioController.java
@@ -5,12 +5,16 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codelogium.portfolioservice.entity.Holding;
+import com.codelogium.portfolioservice.entity.Portfolio;
 import com.codelogium.portfolioservice.service.HoldingService;
+import com.codelogium.portfolioservice.service.PortfolioService;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -19,8 +23,14 @@ import lombok.AllArgsConstructor;
 public class PortfolioController {
 
     private HoldingService holdingService;
+    private PortfolioService portfolioService;
 
     @PostMapping("/")
+    public ResponseEntity<Portfolio> addPortfolio(@RequestBody @Valid Portfolio portfolio) {
+        return new ResponseEntity<>(portfolioService.addPortfolio(portfolio), HttpStatus.valueOf(200));
+    }
+
+    @PostMapping("/{id}/holding")
     public ResponseEntity<Holding> addHolding(@PathVariable Holding holding) {
         return new ResponseEntity<>(holdingService.addHolding(holding), HttpStatus.valueOf(200));
     }
