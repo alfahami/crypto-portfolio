6347be3781f45fcc9665e61e2453ad47a27678e7
AL-FAHAMI <altoihir@gmail.com>
2025-02-06 04:28:06 +0100

    feat: add get user handler + no args constructor on User

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/User.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/User.java
@@ -3,7 +3,6 @@ package com.codelogium.portfolioservice.entity;
 import java.time.LocalDate;
 import java.util.Set;
 
-import jakarta.annotation.Nonnull;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,11 +12,13 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
 @RequiredArgsConstructor
 @Table(name = "users")
 public class User {
@@ -28,9 +29,9 @@ public class User {
     @NonNull
     @NotBlank(message = "Full Name cannot be blank")
     private String fullName;
-    @Nonnull
+    @NonNull
     private LocalDate birthDate;
-    @Nonnull
+    @NonNull
     @NotBlank(message = "Profession cannot be blank")
     private String profession;
 
--- portfolioservice/src/main/java/com/codelogium/portfolioservice/web/PortfolioController.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/web/PortfolioController.java
@@ -3,6 +3,7 @@ package com.codelogium.portfolioservice.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,26 +12,39 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.codelogium.portfolioservice.entity.Holding;
 import com.codelogium.portfolioservice.entity.Portfolio;
+import com.codelogium.portfolioservice.entity.User;
 import com.codelogium.portfolioservice.service.HoldingService;
 import com.codelogium.portfolioservice.service.PortfolioService;
+import com.codelogium.portfolioservice.service.UserService;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(value = "/portfolios", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
 public class PortfolioController {
 
     private HoldingService holdingService;
     private PortfolioService portfolioService;
+    private UserService userService;
 
     @PostMapping("/")
+    public ResponseEntity<User> addUser(@RequestBody @Valid User user) {
+        return new ResponseEntity<>(userService.addUser(user), HttpStatus.valueOf(200));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userService.getUser(id), HttpStatus.valueOf(200));
+    }
+
+    @PostMapping("/{id}/portfolios/")
     public ResponseEntity<Portfolio> addPortfolio(@RequestBody @Valid Portfolio portfolio) {
         return new ResponseEntity<>(portfolioService.addPortfolio(portfolio), HttpStatus.valueOf(200));
     }
 
-    @PostMapping("/{id}/holding")
+    @PostMapping("/portfolios/{id}/holding")
     public ResponseEntity<Holding> addHolding(@PathVariable Holding holding) {
         return new ResponseEntity<>(holdingService.addHolding(holding), HttpStatus.valueOf(200));
     }
