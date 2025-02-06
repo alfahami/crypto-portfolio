1f4d21cb1a73964779dd156d1ab9b13dc617fc91
AL-FAHAMI <altoihir@gmail.com>
2025-02-06 03:45:06 +0100

    feat: implement all unwrap object method

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/service/HoldingServiceImp.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/service/HoldingServiceImp.java
@@ -2,9 +2,12 @@ package com.codelogium.portfolioservice.service;
 
 import java.util.Optional;
 
+import javax.swing.text.html.Option;
+
 import org.springframework.stereotype.Service;
 
 import com.codelogium.portfolioservice.entity.Holding;
+import com.codelogium.portfolioservice.exception.EntityNotFoundException;
 import com.codelogium.portfolioservice.respositry.HoldingRepository;
 
 import lombok.AllArgsConstructor;
@@ -18,5 +21,10 @@ public class HoldingServiceImp implements HoldingService {
     @Override
     public Holding addHolding(Holding holding) {
         return this.holdingRepository.save(holding);
+    }
+
+    public static Holding unwrapHolding(Long id, Optional<Holding> optHolding) {
+        if(optHolding.isPresent()) return optHolding.get();
+        else throw new EntityNotFoundException(id, Holding.class);
     }
 }
--- portfolioservice/src/main/java/com/codelogium/portfolioservice/service/UserServiceImp.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/service/UserServiceImp.java
@@ -1,8 +1,11 @@
 package com.codelogium.portfolioservice.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.codelogium.portfolioservice.entity.User;
+import com.codelogium.portfolioservice.exception.EntityNotFoundException;
 import com.codelogium.portfolioservice.respositry.UserRepository;
 
 import lombok.AllArgsConstructor;
@@ -16,6 +19,17 @@ public class UserServiceImp implements UserService {
     @Override
     public User addUser(User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @Override
+    public User addPortfolioToUser(Long userId, Long portfolioId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static User unwrapUser(Long id, Optional<User> optionalUser) {
+        if(optionalUser.isPresent()) return optionalUser.get();
+        else throw new EntityNotFoundException(id, User.class);
     }
     
 
