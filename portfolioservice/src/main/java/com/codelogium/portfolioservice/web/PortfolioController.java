0786b975ca9646d118322e7b4764b79ed35afa15
AL-FAHAMI <altoihir@gmail.com>
2025-02-06 02:52:18 +0100

    feat: add add holding handler

---
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/web/PortfolioController.java
@@ -0,0 +1,30 @@
+package com.codelogium.portfolioservice.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codelogium.portfolioservice.entity.Holding;
+import com.codelogium.portfolioservice.service.HoldingService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/portfolios", produces = MediaType.APPLICATION_JSON_VALUE)
+public class PortfolioController {
+
+    private HoldingService holdingService;
+
+    @PostMapping("/")
+    public ResponseEntity<Holding> addHolding(@PathVariable Holding holding) {
+        return new ResponseEntity<>(holdingService.addHolding(holding), HttpStatus.valueOf(200));
+    }
+
+
+    
+}
