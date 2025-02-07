b34040394708f46266125c6f5aa446e9678a1791
AL-FAHAMI <altoihir@gmail.com>
2025-02-07 23:25:47 +0100

    fix: fetch holdings, portfolios by its linked id

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/respositry/HoldingRepository.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/respositry/HoldingRepository.java
@@ -1,9 +1,13 @@
 package com.codelogium.portfolioservice.respositry;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codelogium.portfolioservice.entity.Holding;
 
 public interface HoldingRepository extends JpaRepository<Holding, Long> {
     
+    // Fetched all holdings that belongs to a portfolio by portfolio id
+    List<Holding> findByPortfolioId(Long id);
 }
--- portfolioservice/src/main/java/com/codelogium/portfolioservice/respositry/PortfolioRespository.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/respositry/PortfolioRespository.java
@@ -1,9 +1,12 @@
 package com.codelogium.portfolioservice.respositry;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.codelogium.portfolioservice.entity.Portfolio;
 
 public interface PortfolioRespository extends JpaRepository<Portfolio, Long> {
-    
+    // Fetches all portfolio that belongs to a user by user id
+    List<Portfolio> findByUserId(Long userId);
 }
