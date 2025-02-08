c6b11af246f99521939afe92a09a4002faed7ee4
AL-FAHAMI <altoihir@gmail.com>
2025-02-08 15:57:39 +0100

    refractor: first name and last name proper fields

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/User.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/User.java
@@ -32,13 +32,17 @@ public class User {
     private Long id;
 
     @NonNull
-    @NotBlank(message = "Full Name cannot be blank")
-    private String fullName;
+    @NotBlank(message = "First name cannot be blank")
+    private String firstName;
+
+    @NonNull
+    @NotBlank(message = "Last name cannot be blank")
+    private String lastName;
 
     @Past
     @NonNull
     private LocalDate birthDate;
-    
+
     @NonNull
     @NotBlank(message = "Profession cannot be blank")
     private String profession;
