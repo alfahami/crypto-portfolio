fc4ed798c7e513edb2526626824bcf04a3fd9c2b
AL-FAHAMI <altoihir@gmail.com>
2025-02-08 23:35:52 +0100

    feat: add user dto

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/dto/UserDTO.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/dto/UserDTO.java
@@ -1,7 +1,10 @@
 package com.codelogium.portfolioservice.dto;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import com.codelogium.portfolioservice.entity.Portfolio;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import lombok.Getter;
@@ -11,6 +14,9 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDTO {
+
+    @NonNull
+    private Long id;
 
     @NonNull
     @NotBlank(message = "First name cannot be blank")
@@ -27,4 +33,6 @@ public class UserDTO {
     @NonNull
     @NotBlank(message = "Profession cannot be blank")
     private String profession;
+
+    private Set<Portfolio> portfolios = new HashSet<>();
 }
