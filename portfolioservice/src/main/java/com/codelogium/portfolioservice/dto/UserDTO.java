abecf7044d197469a7e2de25fae56a5efad2c048
AL-FAHAMI <altoihir@gmail.com>
2025-02-08 15:57:02 +0100

    feat: add validation constraints

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/dto/UserDTO.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/dto/UserDTO.java
@@ -2,14 +2,29 @@ package com.codelogium.portfolioservice.dto;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class UserDTO {
-    private String firstname;
-    private String lastname;
+
+    @NonNull
+    @NotBlank(message = "First name cannot be blank")
+    private String firstName;
+
+    @NonNull
+    @NotBlank(message = "Last name cannot be blank")
+    private String lastName;
+
+    @Past
+    @NonNull
     private LocalDate birthDate;
+
+    @NonNull
+    @NotBlank(message = "Profession cannot be blank")
     private String profession;
 }
