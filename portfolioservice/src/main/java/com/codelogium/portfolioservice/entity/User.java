023dd963b06ef5605a4e51228de719ba850c9eb3
AL-FAHAMI <altoihir@gmail.com>
2025-02-08 09:52:20 +0100

    refractor: rm unsed imports

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/User.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/entity/User.java
@@ -2,7 +2,6 @@ package com.codelogium.portfolioservice.entity;
 
 import java.time.LocalDate;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
