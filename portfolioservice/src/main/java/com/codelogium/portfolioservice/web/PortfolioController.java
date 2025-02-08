b74bba9b27f194c2496cdf67aa86d8cfba6b3cce
AL-FAHAMI <altoihir@gmail.com>
2025-02-08 09:55:34 +0100

    feat: implement create portfolio handler

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/web/PortfolioController.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/web/PortfolioController.java
@@ -21,8 +21,8 @@ public class PortfolioController {
 
     private PortfolioService portfolioService;
 
-    @PostMapping
-    public ResponseEntity<Portfolio> addPortfolio(@PathVariable Long userId, @RequestBody @Valid Portfolio portfolio) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Portfolio> createPortfolio(@PathVariable Long userId, @RequestBody @Valid Portfolio portfolio) {
         return new ResponseEntity<>(portfolioService.createPortfolio(userId, portfolio), HttpStatus.valueOf(200));
     }
 
