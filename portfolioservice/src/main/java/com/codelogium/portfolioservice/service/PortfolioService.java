342b6159610138a7ad3476eb8d0237b1b6f32b5b
AL-FAHAMI <altoihir@gmail.com>
2025-02-10 11:50:52 +0100

    feat: implement partially update portfolio

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/service/PortfolioService.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/service/PortfolioService.java
@@ -5,6 +5,8 @@ import com.codelogium.portfolioservice.entity.Portfolio;
 public interface PortfolioService {
     Portfolio createPortfolio(Long userId, Portfolio portfolio);
     Portfolio getPortfolio(Long id);
+    Portfolio updatePortfolio(Long id, Portfolio newPortfolio);
     void removePortfolio(Long id);
+
 } 
 
--- portfolioservice/src/main/java/com/codelogium/portfolioservice/service/PortfolioServiceImp.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/service/PortfolioServiceImp.java
@@ -1,8 +1,11 @@
 package com.codelogium.portfolioservice.service;
 
+import static com.codelogium.portfolioservice.util.EntityUtils.*;
+
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.codelogium.portfolioservice.entity.Portfolio;
 import com.codelogium.portfolioservice.entity.User;
@@ -23,6 +26,17 @@ public class PortfolioServiceImp implements PortfolioService {
     public Portfolio createPortfolio(Long userId, Portfolio portfolio) {
         User user = UserServiceImp.unwrapUser(userId, userRepository.findById(userId));
         portfolio.setUser(user);
+        return portfolioRespository.save(portfolio);
+    }
+
+    @Transactional
+    @Override
+    public Portfolio updatePortfolio(Long id, Portfolio newPortfolio) {
+
+        Portfolio portfolio = unwrapPortfolio(id, portfolioRespository.findById(id));
+        newPortfolio.setId(portfolio.getId()); // Ignore ID of request body
+        updateIfNotNull(portfolio::setName, newPortfolio.getName());
+
         return portfolioRespository.save(portfolio);
     }
 
