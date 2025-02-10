67e6f4c5ffc469222aaf7a984c41f3dac487a4c4
AL-FAHAMI <altoihir@gmail.com>
2025-02-10 18:44:43 +0100

    feat: get portfolio by id ensuring it belongs to the specified user

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/respositry/PortfolioRepository.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/respositry/PortfolioRepository.java
@@ -1,6 +1,7 @@
 package com.codelogium.portfolioservice.respositry;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,6 @@ import com.codelogium.portfolioservice.entity.Portfolio;
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
     // Fetches all portfolio that belongs to a user by user id
     List<Portfolio> findByUserId(Long userId);
+    // Retrieves a portfolio by its ID, ensuring it belongs to the specified user.
+    Optional<Portfolio> findByIdAndUserId(Long portfolioId, Long userId);
 }
