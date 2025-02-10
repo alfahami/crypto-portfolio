004462c557ef2c589933064c67a7ad9c0ed14c0d
AL-FAHAMI <altoihir@gmail.com>
2025-02-10 12:30:14 +0100

    feat: add remove holding handler

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/web/HoldingController.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/web/HoldingController.java
@@ -3,6 +3,7 @@ package com.codelogium.portfolioservice.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,12 @@ public class HoldingController {
     @PatchMapping("/{id}")
     public ResponseEntity<Holding> updateHolding(@PathVariable Long id, @RequestBody @Valid Holding holding) {
         return new ResponseEntity<>(holdingService.updateHolding(id, holding), HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> removeHolding(@PathVariable Long id) {
+        holdingService.removeHolding(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
