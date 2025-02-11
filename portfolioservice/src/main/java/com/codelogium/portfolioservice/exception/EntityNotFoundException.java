67fa1fb35770015bbe966f564b5e788edcadd0f1
AL-FAHAMI <altoihir@gmail.com>
2025-02-11 20:03:46 +0100

    refractor: more appealing exception message

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/exception/EntityNotFoundException.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/exception/EntityNotFoundException.java
@@ -3,7 +3,7 @@ package com.codelogium.portfolioservice.exception;
 public class EntityNotFoundException extends RuntimeException {
 
     public EntityNotFoundException(Long id, Class<?> entityClass) {
-        super("The " + entityClass.getSimpleName().toLowerCase() + " with the id " + id + " is not found in our records");
+        super("The " + entityClass.getSimpleName().toLowerCase() + " with the id " + id + " is not found.");
     }
     
 }
