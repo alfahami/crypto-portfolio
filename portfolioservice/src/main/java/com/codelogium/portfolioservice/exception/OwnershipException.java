ee92b6232722f09366680e754ab8222a59cf10c4
AL-FAHAMI <altoihir@gmail.com>
2025-02-11 20:03:14 +0100

    refractor: delete ownership exception

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/exception/OwnershipException.java
+++
@@ -1,8 +0,0 @@
-package com.codelogium.portfolioservice.exception;
-
-public class OwnershipException extends RuntimeException {
-    
-    public OwnershipException(Class<?> entityClass) {
-        super("No onwership of " + entityClass.getSimpleName().toLowerCase());
-    }
-}
