70ab495d5174bb24f95cb8242863aae45a7d7786
AL-FAHAMI <altoihir@gmail.com>
2025-02-12 21:41:53 +0100

    feat: add error response pojo

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/exception/ErrorResponse.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/exception/ErrorResponse.java
@@ -1,5 +1,24 @@
 package com.codelogium.portfolioservice.exception;
 
+import java.time.Instant;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ErrorResponse {
+    
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC")
+    private Instant timestamp;
+    private List<String> messages;
+
+    public ErrorResponse(List<String> messages) {
+        this.timestamp = Instant.now();
+        this.messages = messages;
+    }
     
 }
