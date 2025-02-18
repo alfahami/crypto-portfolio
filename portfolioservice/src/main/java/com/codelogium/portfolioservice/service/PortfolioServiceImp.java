019ac8c5775ac5afc052b8781fb233ae605de7ce
AL-FAHAMI <altoihir@gmail.com>
2025-02-18 18:20:43 +0100

    feat: implement error handling w/ exchange rate service

--- portfolioservice/src/main/java/com/codelogium/portfolioservice/service/PortfolioServiceImp.java
+++ portfolioservice/src/main/java/com/codelogium/portfolioservice/service/PortfolioServiceImp.java
@@ -6,19 +6,23 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import com.codelogium.portfolioservice.entity.Holding;
 import com.codelogium.portfolioservice.entity.Portfolio;
 import com.codelogium.portfolioservice.entity.User;
+import com.codelogium.portfolioservice.exception.ExchangeRateException;
 import com.codelogium.portfolioservice.exception.ResourceNotFoundException;
 import com.codelogium.portfolioservice.respositry.PortfolioRepository;
 import com.codelogium.portfolioservice.respositry.UserRepository;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import lombok.AllArgsConstructor;
+import reactor.core.publisher.Mono;
 
 @Service
 @AllArgsConstructor
@@ -82,27 +86,41 @@ public class PortfolioServiceImp implements PortfolioService {
 
         // initialize the result to accumulate the total valuation
         BigDecimal result = BigDecimal.ZERO;
-        if(portfolio.getHoldings() != null) {
+        if (portfolio.getHoldings() != null) {
             for (Holding holding : portfolio.getHoldings()) {
-                result = result.add(holding.getAmount().multiply(getCurrentPrice(holding.getSymbol(), base)));    
+                result = result.add(holding.getAmount().multiply(getCurrentPrice(holding.getSymbol(), base)));
             }
             return result;
-        }
-        else throw new ResourceNotFoundException("Portfolio doesn't have holdings yet");
+        } else
+            throw new ResourceNotFoundException("Portfolio doesn't have holdings yet");
     }
 
     // Retrieves the current price of crypto using exchangerateservice local api
     public BigDecimal getCurrentPrice(String symbol, String base) {
+        try {
         JsonNode data = webClient.get().uri(uriBuilder -> uriBuilder.path("/exchange-rate")
         .queryParam("symbol", symbol)
         .queryParam("base", base)
         .build())
         .retrieve()
+        .onStatus(HttpStatusCode::isError, clientResponse -> Mono.error(new ExchangeRateException("Failed to fetch exchange rate")))
         .bodyToMono(JsonNode.class)
-        .block();
+        .block(); // Bolocking for simplicity (avoid in reactive flows)
 
         String price = data.get("price").toString();
+        
+        if(data == null || price == null) {
+            throw new ExchangeRateException("Invalid Exchange rate response");
+        }
+
         return new BigDecimal(price);
+        // WebClient-Specific Exceptions (API response errors)
+        } catch(WebClientResponseException ex) {
+            throw new ExchangeRateException("Exchange rate service error: " + ex.getMessage(), ex);
+        // Catch All Other Exceptions (e.g., NullPointerException, IllegalStateException)
+        } catch(Exception ex) {
+            throw new ExchangeRateException("Unexpected error fetching exchange rate", ex);
+        }
     }
 
     // Other relation down level might need to check for user existance
