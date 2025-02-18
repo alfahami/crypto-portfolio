6dd80c83d09f89c6e4bf5a5edfd006743657a892
AL-FAHAMI <altoihir@gmail.com>
2025-02-18 20:02:13 +0100

    test: add get valuation successfully

--- portfolioservice/src/test/java/com/codelogium/portfolioservice/PortfolioServiceTest.java
+++ portfolioservice/src/test/java/com/codelogium/portfolioservice/PortfolioServiceTest.java
@@ -1,10 +1,13 @@
 package com.codelogium.portfolioservice;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
@@ -12,9 +15,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.codelogium.portfolioservice.entity.Holding;
 import com.codelogium.portfolioservice.entity.Portfolio;
 import com.codelogium.portfolioservice.entity.User;
 import com.codelogium.portfolioservice.respositry.PortfolioRepository;
@@ -22,15 +29,16 @@ import com.codelogium.portfolioservice.respositry.UserRepository;
 import com.codelogium.portfolioservice.service.PortfolioService;
 import com.codelogium.portfolioservice.service.PortfolioServiceImp;
 
+import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 
 @ExtendWith(SpringExtension.class)
 public class PortfolioServiceTest {
-    
+
     // Initialze Mock web server
     private static MockWebServer mockWebServer;
-    private  PortfolioService portfolioservice;
-    
+    private PortfolioService portfolioservice;
+
     @Mock
     private UserRepository userRepository;
 
@@ -43,7 +51,7 @@ public class PortfolioServiceTest {
         mockWebServer.start(8585);
 
         WebClient mockedWebClient = WebClient.builder().baseUrl(mockWebServer.url("/").toString())
-        .build();
+                .build();
 
         portfolioservice = new PortfolioServiceImp(portfolioRepository, userRepository, mockedWebClient);
 
@@ -57,7 +65,36 @@ public class PortfolioServiceTest {
 
     @Test
     void shouldGetValuationSuccessfully() {
+        // Mock
+        User testUser = new User(1L, "John", "Doe", LocalDate.parse("1999-09-24"), "Developer", null);
+
+        Portfolio portfolio = new Portfolio(10L, "Tech Guru Investment", testUser, new ArrayList<>());
+
+        Holding holding1 = new Holding(1L, "BTC", new BigDecimal("21.43"), portfolio);
+        Holding holding2 = new Holding(2L, "ETH", new BigDecimal("45.12"), portfolio);
+
+        portfolio.setHoldings(List.of(holding1, holding2));
+
+        when(userRepository.existsById(1L)).thenReturn(true);
+        when(portfolioRepository.findByIdAndUserId(10L, 1L)).thenReturn(Optional.of(portfolio));
         
+        // Mock API for BTC
+        mockWebServer.enqueue(
+            new MockResponse().setResponseCode(HttpStatus.OK.value())
+            .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .setBody("{\"price\": 1259.54}"));
+
+        // Mock API for ETH
+        mockWebServer.enqueue(
+            new MockResponse().setResponseCode(HttpStatus.OK.value())
+            .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .setBody("{\"price\": 895.35}"));
+
+        // AcT
+        BigDecimal result = portfolioservice.valuation(1L, 10L, "EUR");
+
+        // Assert
+        assertEquals(new BigDecimal("67390.1342"), result); 
     }
 
     @Test
@@ -74,7 +111,7 @@ public class PortfolioServiceTest {
         // Assert
         assertNotNull(createdPortfolio);
         assertEquals("Tech Guru Investment", createdPortfolio.getName());
-        verify(portfolioRepository, times(1)).save(any(Portfolio.class));   
+        verify(portfolioRepository, times(1)).save(any(Portfolio.class));
     }
 
     @Test
@@ -89,24 +126,24 @@ public class PortfolioServiceTest {
 
     @Test
     void shouldRetrievePortfolioSuccessfully() {
-        //Mock 
+        // Mock
         User testUser = new User(1L, "John", "Doe", LocalDate.parse("1999-09-24"), "Developer", null);
         Portfolio portfolio = new Portfolio(1L, "Tech Guru Investment", testUser, new ArrayList<>());
 
         when(userRepository.findById(1L)).thenReturn(Optional.of(testUser));
         when(portfolioRepository.findByIdAndUserId(1L, 1L)).thenReturn(Optional.of((portfolio)));
-        
+
         // Act
         Portfolio result = portfolioservice.retrievePortfolio(1L, 1L);
 
-        //Assert
+        // Assert
         assertEquals(result.getId(), portfolio.getId());
         assertEquals(result.getName(), portfolio.getName());
     }
 
     @Test
     void shouldFailToRetrievePortfolioWhenPortfolioNotExist() {
-        //Mock 
+        // Mock
         User testUser = new User(1L, "John", "Doe", LocalDate.parse("1999-09-24"), "Developer", null);
         when(userRepository.findById(1L)).thenReturn(Optional.of(testUser));
 
@@ -131,7 +168,7 @@ public class PortfolioServiceTest {
 
         retrievedPortfolio.setName("Medium Tech Stock");
         retrievedPortfolio.setId(999L); // tampering the ID in order to test if it gets ignored by the service
-        
+
         when(portfolioRepository.save(any(Portfolio.class))).thenReturn(retrievedPortfolio);
 
         // Act
