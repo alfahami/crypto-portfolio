4846d19638ebf6bf5d391a0377a524b942880305
AL-FAHAMI <altoihir@gmail.com>
2025-02-19 21:46:43 +0100

    test: set up integration test base class

--- portfolioservice/src/test/java/com/codelogium/portfolioservice/PortfolioEndToEndTest.java
+++ portfolioservice/src/test/java/com/codelogium/portfolioservice/PortfolioEndToEndTest.java
@@ -1,6 +1,28 @@
 package com.codelogium.portfolioservice;
 
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
+import com.codelogium.portfolioservice.service.PortfolioServiceImp;
+import com.codelogium.portfolioservice.web.PortfolioController;
+
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {PortfolioController.class, PortfolioServiceImp.class})
+@WebMvcTest
 public class PortfolioEndToEndTest {
     
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void postPrtfolio() {
+        
+    }
+
+
+
 }
